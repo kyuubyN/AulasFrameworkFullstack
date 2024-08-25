62fbbb012bd6fa9b17bab1386d3c6fe684bf66f3
Kaue - kyuubyN <102341451+kyuubyN@users.noreply.github.com>
2024-08-25 14:34:16 -0300

    Att v1.2
    Adição do setor destaques com uma nova função de product slider

--- Atividade1/static/script.js
+++ Atividade1/static/script.js
@@ -6,3 +6,30 @@ document.addEventListener("visibilitychange", function() {
         video.play();
     }
 });
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+    const visibleSlides = 3; // Número de slides visíveis de uma vez
+
+    if (index >= totalSlides / visibleSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = Math.floor((totalSlides - 1) / visibleSlides);
+    } else {
+        currentSlide = index;
+    }
+
+    const slider = document.querySelector('.slider');
+    slider.style.transform = `translateX(-${currentSlide * 100 / visibleSlides}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
